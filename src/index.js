03b59810a3d66259aadb781d8699e705e11c141e
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2021-08-03 16:06:09 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 /* eslint-disable no-console */
 
-console.log(1122334)
+function laterExecute() {
+  if (window.location.pathname.startsWith('/k/20565/show') && !window.location.hash.includes('edit')) {
+    console.log('in1 111i')
+    const stringPassRecordKey = kintone.app.record.get().record.record_key.value
+    const stringPassTitle = kintone.app.record.get().record.title.value
+    const eleCopyRecordKeyButton = document.createElement('BUTTON')
+    eleCopyRecordKeyButton.innerText = 'cp'
+    eleCopyRecordKeyButton.style.position = 'relative'
+    eleCopyRecordKeyButton.style.left = '12px'
+    const eleRecordKey = kintone.app.record.getFieldElement('record_key')
+    eleCopyRecordKeyButton.onclick = () => {
+      // GM_openInTab('https://cndevqpofif.cybozu.cn/k/21/edit', { active: true })
+      // console.log(GM_info.script.uuid)
+      const bc = new BroadcastChannel('postme')
+      setTimeout(() => {
+        bc.postMessage({ refKey: stringPassRecordKey, content: stringPassTitle })
+      }, 2000)
+    }
+    eleRecordKey.appendChild(eleCopyRecordKeyButton)
+  }
+
+  if (window.location.pathname.startsWith('/k/46878/edit')) {
+    const bc = new BroadcastChannel('postme')
+    bc.onmessage = (ev) => {
+      console.log(ev)
+      const editingRecord = kintone.app.record.get().record
+      // console.log(editingRecord)
+      editingRecord.subtable.value[0].value.pj_name.value = ev.data.refKey
+      editingRecord.subtable.value[0].value.pj_content.value = ev.data.content
+      editingRecord.subtable.value[0].value.type.value = '项目常规'
+      editingRecord.subtable.value[0].value.hour.value = 8
+      kintone.app.record.set({ record: editingRecord })
+    }
+  } else {
+    console.log(window.location.href)
+  }
+  if (window.location.href.includes('edit')) {
+    console.log('maybe in edit1')
+  }
+}
 
 setTimeout(() => {
-  window.postMessage({ cmd: 'dosth', msg: 'ok' }, document.URL)
-}, 3000)
+  laterExecute()
+}, 800)
 
-onmessage = (event) => {
-  console.log(event.data)
+onhashchange = () => {
+  laterExecute()
 }
---
+++ src/monkey.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         CybozuDL组内用Task快填日报工数
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  填写日报工数时，常常需要引用task记录，每次都要去找，复制内容等非常反人性。现在此工具在task编号旁边加上一个按钮，解决任务填入日报问题。
+// @author       Bxu
+// @match        https://bozuman.cybozu.com/k/20565/show*
+// @match        https://bozuman.cybozu.com/k/46878/*
+// @match        https://bozuman.s.cybozu.com/k/20565/show*
+// @match        https://bozuman.s.cybozu.com/k/46878/*
+// @icon         https://img.icons8.com/dusk/64/000000/copy.png
+// @grant        GM_openInTab
+// @grant        GM_info
+// ==/UserScript==
+
+;(function () {
+  function laterExecute() {
+    if (window.location.pathname.startsWith('/k/20565/show') && !window.location.hash.includes('edit')) {
+      const stringPassRecordKey = kintone.app.record.get().record.record_key.value
+      const stringPassTitle = kintone.app.record.get().record.title.value
+      const eleCopyRecordKeyButton = document.createElement('BUTTON')
+      eleCopyRecordKeyButton.innerText = '用此Task填写工数'
+      eleCopyRecordKeyButton.style.backgroundColor = '#F78448'
+      eleCopyRecordKeyButton.style.color = '#EEEEEE'
+      eleCopyRecordKeyButton.style.borderColor = '#AAAAAA'
+      eleCopyRecordKeyButton.style.position = 'relative'
+      eleCopyRecordKeyButton.style.left = '12px'
+      const eleRecordKey = kintone.app.record.getFieldElement('record_key')
+      eleCopyRecordKeyButton.onclick = () => {
+        GM_openInTab(`${window.location.origin}/k/46878/edit`, { active: true })
+        const bc = new BroadcastChannel(GM_info.script.uuid)
+        setTimeout(() => {
+          bc.postMessage({ refKey: stringPassRecordKey, content: stringPassTitle })
+        }, 3000)
+      }
+      eleRecordKey.appendChild(eleCopyRecordKeyButton)
+    }
+    if (window.location.pathname.startsWith('/k/46878/edit')) {
+      const bc = new BroadcastChannel(GM_info.script.uuid)
+      bc.onmessage = (ev) => {
+        const editingRecord = kintone.app.record.get().record
+        // console.log(editingRecord)
+        editingRecord.subtable.value[0].value.pj_name.value = ev.data.refKey
+        editingRecord.subtable.value[0].value.pj_content.value = ev.data.content
+        editingRecord.subtable.value[0].value.type.value = '项目常规'
+        editingRecord.subtable.value[0].value.hour.value = 8
+        kintone.app.record.set({ record: editingRecord })
+      }
+    }
+  }
+  setTimeout(() => {
+    laterExecute()
+  }, 800)
+
+  onhashchange = () => {
+    laterExecute()
+  }
+})()
