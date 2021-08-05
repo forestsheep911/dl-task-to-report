05b5ab7059a761410c0c69a7e05c624418172bff
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2021-08-05 13:23:06 +0800

    save

--- src/dl-task-to-report.js
+++ src/dl-task-to-report.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CybozuDL组内用Task快填日报工数
-// @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @namespace    https://github.com/forestsheep911/dl-task-to-report
+// @version      0.0.2
 // @description  填写日报工数时，常常需要引用task记录，每次都要去找，复制内容等非常反人性。现在此工具在task编号旁边加上一个按钮，解决任务填入日报问题。
 // @author       Bxu
 // @match        https://bozuman.cybozu.com/k/20565/show*
@@ -11,18 +11,41 @@
 // @icon         https://img.icons8.com/dusk/64/000000/copy.png
 // @grant        GM_openInTab
 // @grant        GM_info
+// @grant        GM_addStyle
 // ==/UserScript==
 
 ;(function () {
+  GM_addStyle(`.container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    text-align:center;
+    min-height: 100vh;
+
+  }
+
+  .btn {
+    font-family: 'Otomanopee One', sans-serif;
+    border: none;
+    color: white;
+    background-color: rgb(255, 0, 68);
+    border-radius: 4px;
+    box-shadow: inset 0 0 0 0 #f9e506;
+    transition: ease-out 0.3s;
+    outline: none;
+    cursor: pointer;
+  }`)
   function laterExecute() {
     if (window.location.pathname.startsWith('/k/20565/show') && !window.location.hash.includes('edit')) {
       const stringPassRecordKey = kintone.app.record.get().record.record_key.value
       const stringPassTitle = kintone.app.record.get().record.title.value
       const eleCopyRecordKeyButton = document.createElement('BUTTON')
       eleCopyRecordKeyButton.innerText = '用此Task填写工数'
-      eleCopyRecordKeyButton.style.backgroundColor = '#F78448'
-      eleCopyRecordKeyButton.style.color = '#EEEEEE'
-      eleCopyRecordKeyButton.style.borderColor = '#AAAAAA'
+      // eleCopyRecordKeyButton.style.backgroundColor = '#F78448'
+      // eleCopyRecordKeyButton.style.color = '#EEEEEE'
+      // eleCopyRecordKeyButton.style.borderColor = '#AAAAAA'
+      eleCopyRecordKeyButton.setAttribute('class', 'btn')
       eleCopyRecordKeyButton.style.position = 'relative'
       eleCopyRecordKeyButton.style.left = '12px'
       const eleRecordKey = kintone.app.record.getFieldElement('record_key')
--- src/index.js
+++ src/index.js
@@ -1,50 +1,41 @@
 /* eslint-disable no-console */
 
-function laterExecute() {
-  if (window.location.pathname.startsWith('/k/20565/show') && !window.location.hash.includes('edit')) {
-    console.log('in1 111i')
-    const stringPassRecordKey = kintone.app.record.get().record.record_key.value
-    const stringPassTitle = kintone.app.record.get().record.title.value
-    const eleCopyRecordKeyButton = document.createElement('BUTTON')
-    eleCopyRecordKeyButton.innerText = 'cp'
-    eleCopyRecordKeyButton.style.position = 'relative'
-    eleCopyRecordKeyButton.style.left = '12px'
-    const eleRecordKey = kintone.app.record.getFieldElement('record_key')
-    eleCopyRecordKeyButton.onclick = () => {
-      // GM_openInTab('https://cndevqpofif.cybozu.cn/k/21/edit', { active: true })
-      // console.log(GM_info.script.uuid)
-      const bc = new BroadcastChannel('postme')
-      setTimeout(() => {
-        bc.postMessage({ refKey: stringPassRecordKey, content: stringPassTitle })
-      }, 2000)
-    }
-    eleRecordKey.appendChild(eleCopyRecordKeyButton)
-  }
+const params = {
+  app: kintone.app.getId(),
+}
 
-  if (window.location.pathname.startsWith('/k/46878/edit')) {
-    const bc = new BroadcastChannel('postme')
-    bc.onmessage = (ev) => {
-      console.log(ev)
-      const editingRecord = kintone.app.record.get().record
-      // console.log(editingRecord)
-      editingRecord.subtable.value[0].value.pj_name.value = ev.data.refKey
-      editingRecord.subtable.value[0].value.pj_content.value = ev.data.content
-      editingRecord.subtable.value[0].value.type.value = '项目常规'
-      editingRecord.subtable.value[0].value.hour.value = 8
-      kintone.app.record.set({ record: editingRecord })
+async function getFields() {
+  const rs = await kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params)
+  return rs
+}
+
+async function mainWork() {
+  const objFields = await getFields()
+  const eleCommonLabels = document.querySelectorAll('.control-label-gaia, .group-label-gaia, .subtable-label-gaia')
+  console.log(eleCommonLabels)
+  for (let i = 0; i < eleCommonLabels.length; i += 1) {
+    // console.log(eleCommonLabels[i].innerText)
+    eleCommonLabels[i].onmouseover = () => {
+      const eleFieldCode = document.createElement('span')
+      eleFieldCode.style.marginLeft = '20px'
+      // console.log(eleCommonLabels[i])
+      Object.keys(objFields.properties).forEach((key) => {
+        if (eleCommonLabels[i].innerText === objFields.properties[key].label) {
+          console.log(objFields.properties[key])
+          const textnodeFieldCodeContent = document.createElement('span')
+          textnodeFieldCodeContent.innerText = objFields.properties[key].code
+          textnodeFieldCodeContent.style.marginLeft = '20px'
+          eleFieldCode.appendChild(textnodeFieldCodeContent)
+        }
+      })
+      eleCommonLabels[i].appendChild(eleFieldCode)
     }
-  } else {
-    console.log(window.location.href)
-  }
-  if (window.location.href.includes('edit')) {
-    console.log('maybe in edit1')
+    eleCommonLabels[i].onmouseout = () => {
+      for (let j = 1; j < eleCommonLabels[i].childNodes.length; j += 1) {
+        eleCommonLabels[i].childNodes[j].remove()
+      }
+    }
   }
 }
 
-setTimeout(() => {
-  laterExecute()
-}, 800)
-
-onhashchange = () => {
-  laterExecute()
-}
+mainWork()
