2036720bca5d30ff16c00d9fb16ec7d86d4975ed
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2021-08-05 13:44:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,19 +12,16 @@ async function getFields() {
 async function mainWork() {
   const objFields = await getFields()
   const eleCommonLabels = document.querySelectorAll('.control-label-gaia, .group-label-gaia, .subtable-label-gaia')
-  console.log(eleCommonLabels)
   for (let i = 0; i < eleCommonLabels.length; i += 1) {
-    // console.log(eleCommonLabels[i].innerText)
     eleCommonLabels[i].onmouseover = () => {
       const eleFieldCode = document.createElement('span')
       eleFieldCode.style.marginLeft = '20px'
-      // console.log(eleCommonLabels[i])
       Object.keys(objFields.properties).forEach((key) => {
         if (eleCommonLabels[i].innerText === objFields.properties[key].label) {
-          console.log(objFields.properties[key])
           const textnodeFieldCodeContent = document.createElement('span')
           textnodeFieldCodeContent.innerText = objFields.properties[key].code
           textnodeFieldCodeContent.style.marginLeft = '20px'
+          textnodeFieldCodeContent.style.color = '#e35db6'
           eleFieldCode.appendChild(textnodeFieldCodeContent)
         }
       })
